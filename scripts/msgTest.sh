3e8571daef185854bed892ace97aa91dc1d4b67e
John <jfelten@pop-os.localdomain>
2020-06-15 08:11:44 -0700

    fix messaging

--- scripts/msgTest.sh
+++ scripts/msgTest.sh
@@ -4,7 +4,25 @@
 
 PAYLOAD=$( cat <<EOT
 {
-    "text": "Hello, world."
+  "blocks": [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": ":thumbsup: gitea success\n\nRepo: <https://git.keyporttech.com:30243/keyporttech/k8sCI|keyporttech/k8sCI>\n\n<https://dashboard.cicd.keyporttech.com:30243/#/namespaces/cicd/pipelineruns/build-pipeline-run-k58wr-ci-bzkv4-pod-6grpv|build information>"
+      }
+    },
+    {
+      "type": "divider"
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "test"
+      }
+    }
+  ]
 }
 EOT
 );
